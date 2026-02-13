66c7d23b670961eb13fa8684ae8815a646afdc1e
Madhurirao95 <madhurirao95@gmail.com>
2026-02-13 14:17:32 -0500

    Removing sensitive information in dbcontext

--- DREAMHOMES/Models/Repository/Db_Context/DreamhomesContext.cs
+++ DREAMHOMES/Models/Repository/Db_Context/DreamhomesContext.cs
@@ -15,10 +15,6 @@ public partial class DreamhomesContext : IdentityDbContext<ApplicationUser>
     {
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=SRINIVASMADHURI\\SQLEXPRESS01;Database=DreamHomes;Trusted_Connection=True;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
